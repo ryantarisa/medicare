1efce67372a349db5b901f4c9ce7b789159caf10
ryantarisa <99316077+ryantarisa@users.noreply.github.com>
2022-07-28 22:16:17 +0800

    Pull-all-feature (#1)
    * [FP-10]- Bikin komponen (#1)
    
    * [FP-8]-see-product (#2)
    
    * [FP-8]-fitur see product
    
    * see-product
    
    tambah fitu see product dan gitignore
    
    * update
    
    gitignore
    
    * [FP-4]-login (#3)
    
    * [FP-4] - fitur login
    
    * [FP-4] - fitur login
    
    Co-authored-by: Handono <teddy@teddys-MacBook-Air.local>
    
    * [FP-7] - Forgot password selesai (#4)
    
    * [FP-6-register] - register judul (#5)
    
    Co-authored-by: Ismail <100994517+Ismailrep@users.noreply.github.com>
    Co-authored-by: Handono122 <95142866+Handono122@users.noreply.github.com>
    Co-authored-by: Handono <teddy@teddys-MacBook-Air.local>
    Co-authored-by: handi7 <57352208+handi7@users.noreply.github.com>
    Co-authored-by: rahmafau <110176964+rahmafau@users.noreply.github.com>

---
+++ detailProduct.js
@@ -0,0 +1,7 @@
+console.log("detailProduct.js");
+const detailProduct = {
+    init: function() {
+        console.log("detailProduct.js init");
+    }
+}
+export default detailProduct;
---
+++ seeProducts.js
@@ -0,0 +1,1 @@
+console.log("ini fitur see product");
